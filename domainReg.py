ef2a1d16149fb8d781ed6f6777e7bc433c7a869e
sc-ashik <orionsami160@gmail.com>
2020-05-15 04:08:45 +0600

    up

--- domainReg.py
+++ domainReg.py
@@ -7,86 +7,84 @@ import time
 import sys
 import os
 
-
-#CONFIG
-API_ENDPOINT = "http://localhost:3000/resello"
+# CONFIG
+API_ENDPOINT = "http://domain-reg.test/resello"
 API_KEY = "XXXXXXXXXXXXXXXXX"
 LABEL = "MYLABEL"
-CUSTOMER_ID=45
+CUSTOMER_ID = 45
 NUMBER_OF_REQUEST = 2
 GAP = 0.05
-SLEEP_TIME=30
+SLEEP_TIME = 20
 
 MYSQL_USER = 'root'
 MYSQL_PASS = 'bnm'
 MYSQL_DATABASE = 'resello-domain-reg'
-#END
+# END
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.stdout=open(ROOT_DIR+"/domainReg.log","a")
+sys.stdout = open(ROOT_DIR + "/domainReg.log", "a")
 
 os.environ['TZ'] = 'Asia/Kolkata'
 time.tzset()
 
-log=""
+log = ""
 
-def RunTaskInThread(row, connection, cursor ):
+
+def RunTaskInThread(row, connection, cursor):
     try:
-        print("\n")
-        print(datetime.datetime.now(),": ",row[1]," scheduled for ",row[3])
-        scheduled_time=row[2]
-        while(datetime.datetime.now().timestamp() < scheduled_time):
+        print()
+        print(datetime.datetime.now(), ": ", row[1], " scheduled for ", row[3])
+        scheduled_time = row[2]
+        stop_time = row[4]
+        # if datetime.datetime.now().timestamp()> scheduled_time:
+        #     return
+        while (datetime.datetime.now().timestamp() < scheduled_time):
             continue
-        requested_at=datetime.datetime.now()
-        print(requested_at,": before requesting ",row[1])
+        # requested_at=datetime.datetime.now()
+        # print(requested_at,": before requesting ",row[1])
 
-        success=False
-        for  i in range(NUMBER_OF_REQUEST):
-            res=requests.post(
+        success = False
+        i = 1
+        while True:
+            res = requests.post(
                 API_ENDPOINT,
                 headers={
                     "X-APIKEY": API_KEY,
                     "label": LABEL
                 },
                 json={
-                   "customer": CUSTOMER_ID,
-                   "domain": row[1],
-                   "interval": 12
+                    "customer": CUSTOMER_ID,
+                    "domain": row[1],
+                    "interval": 12
                 }
 
             )
-            print("  ",i, row[1], res.status_code, res.text)
+            print("  ", i, row[1], res.status_code, res.text)
             rjson = res.json()
 
-            success=success or rjson['success']
+            success = success or rjson['success']
+            i = i + 1
+            if datetime.datetime.now().timestamp() > stop_time:
+                break
 
-        received_at=datetime.datetime.now()
-        print(requested_at,": After receiving last response of ",row[1])
+        # received_at=datetime.datetime.now()
+        # print(requested_at,": After receiving last response of ",row[1])
 
-
-
-        sql = "INSERT INTO completed_tasks (domain_name, scheduled_at,requested_at,received_at,response) VALUES (%s, %s, %s, %s, %s  )"
-        response="success:"+str(success)
-        val = (row[1],row[3],requested_at,received_at,response)
+        sql = "INSERT INTO completed_tasks (domain_name,begin_time,end_time,req_count,response) VALUES (%s, %s, %s, %s, %s  )"
+        response = "success:" + str(success)
+        val = (row[1], row[3], row[5], i, response)
         cursor.execute(sql, val)
-
-        sql_Delete_query = """Delete from tasks where id = %s"""
-        taskId = row[0]
-        cursor.execute(sql_Delete_query, (taskId,))
-
         connection.commit()
 
-        print(datetime.datetime.now(),"Finished requesting ", row[1])
+        print(datetime.datetime.now(), "Finished requesting ", row[1])
     except Error as e:
-        print("Error for ",row[1],": ", e)
-
-
+        print("Error for ", row[1], ": ", e)
 
 
 try:
 
-    print("\n")
-    print(datetime.datetime.now(),": cron Job started")
+    print()
+    print(datetime.datetime.now(), ": cron Job started")
 
     time.sleep(SLEEP_TIME)
 
@@ -102,21 +100,27 @@ try:
     if len(records) == 0:
         print("Exiting, no task pending")
         exit()
-    threads=[]
+    threads = []
 
-    print(datetime.datetime.now(),": before queuing")
-    taskCount=0
+    print(datetime.datetime.now(), ": before queuing")
+    taskCount = 0
     for row in records:
-        dif=row[2] - datetime.datetime.now().timestamp();
-        if 60 > dif >= 0:
-           x=threading.Thread(target=RunTaskInThread,args=(row,connection,cursor,))
-           threads.append(x)
-           x.start()
-           taskCount+=1
-    print(datetime.datetime.now(),": after queuing", taskCount," tasks")
+        dif = row[2] - datetime.datetime.now().timestamp();
+        if 65 > dif >= 0:
+            x = threading.Thread(target=RunTaskInThread, args=(row, connection, cursor,))
+            threads.append(x)
+            x.start()
+            taskCount += 1
+
+            sql_Delete_query = """Delete from tasks where id = %s"""
+            taskId = row[0]
+            cursor.execute(sql_Delete_query, (taskId,))
+            connection.commit()
+
+    print(datetime.datetime.now(), ": after queuing", taskCount, " tasks")
     for x in threads:
         x.join()
-    print(datetime.datetime.now(),": cron Job finished")
+    print(datetime.datetime.now(), ": cron Job finished")
 
 except Error as e:
     print("Error reading data from MySQL table", e)
@@ -137,5 +141,3 @@ finally:
 #         }
 #     ]
 # }
-
-
